55f036f51e3a03c4f9f1c0970e832dcf7aa0b4cc
Chris Conover <clcono@gmail.com>
2011-12-01 15:38:52 -0500

    Adjusted announcements styling

--- includes/announcements.php
+++ includes/announcements.php
@@ -4,14 +4,15 @@ $rss = fetch_feed(ANNOUNCEMENTS_RSS_URL);
 if(!is_wp_error($rss)) {
 	$rss_items = $rss->get_items(0, $rss->get_item_quantity(4));
 	?>
-	<span style="font-size:150%;font-weight:100;display:block;padding-bottom:40px;">UCF Announcements</span>
+	<p style="font-size:150%;font-weight:100;display:block;">UCF Announcements</p>
 	<?
 	foreach($rss_items as $item) { ?>
-		<p style="padding-bottom:20px;padding-top:0;margin:0;">
+		<p style="padding-top:0;margin:0;">
 			<a style="font-weight:100;color:#9d1a1a;" href="<?=$item->get_permalink()?>">
 				<?=$item->get_title()?>
 			</a>
 		</p>
+		<br />
 	<?
 	}
 }
