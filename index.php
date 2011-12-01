3ab6a13e6fb6227ed1764b2d1f819b9b8ef05529
Christopher Conover <clcono@gmail.com>
2011-12-01 10:07:34 -0500

    Initial layout with events

--- index.php
+++ index.php
@@ -1,25 +1,293 @@
-<?php get_header();?>
-<div class="page-content" id="post-list">
-	<div class="span-18">
-		<?php while(have_posts()): the_post();?>
-		<article class="<?=$post->post_status?>">
-			<h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
-			<div class="meta">
-				<span class="date"><?php the_time("F j, Y");?></span>
-				<span class="author">by <?php the_author_posts_link();?></span>
-			</div>
-			<div class="summary">
-				<?php the_excerpt();?>
-			</div>
-		</article>
-		<?php endwhile;?>
-	</div>
-	<div id="sidebar" class="span-6 last">
-		<?=get_sidebar();?>
-	</div>
-	
-	<div id="below-the-fold" class="clear">
-		<?php get_template_part('includes/below-the-fold'); ?>
-	</div>
-</div>
-<?php get_footer();?>
+<html lang="en" xmlns:v="urn:schemas-microsoft-com:vml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<title>Good Morning UCF</title>
+		<style type="text/css">
+			html, body { margin:0px; padding:0; background-color:#FFF; color:#333; font-family:Helvetica, sans-serif; }
+			v\:* { behavior: url(#default#VML); display:inline-block}
+		</style>
+	</head>
+	<body bgcolor="#FFF">
+		<div id="main" style="padding: 0;">
+			<table width="600" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 600px; margin:20px auto 0 auto; background-color:#FFF;border-bottom:1px solid #ddd;">
+				<tr style="text-align:center;border-bottom:1px solid #ddd;">
+					<td style="font-weight:bold;border-right:1px solid #ddd;width:160px;">
+						<table style="padding-left:20px">
+							<tr>
+								<td style="font-size:90%;">TODAY</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td style="width:60px;"><img src="<?=bloginfo('stylesheet_directory')?>/static/img/gmucf-weather-today.png" /></td>
+								<td>
+									<span style="font-size:160%;position:relative;left:5px;">71&deg;</span>
+									<span style="font-weight:100;display:block;">High</span>
+								</td>
+							</tr>
+						</table>
+					</td>
+					<td style="font-weight:bold;border-right:1px solid #ddd;width:225px;">
+						<table style="margin-left:20px;">
+							<tr>
+								<td style="font-size:90%;">TONIGHT</td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td><img src="<?=bloginfo('stylesheet_directory')?>/static/img/gmucf-weather-tonight.png" /></td>
+								<td style="text-align:center;">
+									<span style="font-size:160%;position:relative;left:5px;">53&deg;</span>
+									<span style="font-weight:100;display:block;">Low</span>
+								</td>
+								<td style="text-align:left;font-size:80%;font-weight:100;padding-left:17px;">
+									<a style="color:#333;" href="www.weather.com/weather/today/Oviedo+FL+USFL0378">More<br />Weather</a>
+								</td>
+							</tr>
+						</table>
+					</td>
+					<td style="border-right:1px solid #ddd;">
+						<span style="font-size:110%;letter-spacing:1px;display:block;margin-bottom:7px;">THURSDAY</span>
+						<span style="font-size:135%;letter-spacing:1.2px;font-weight:100;">October 20</span>
+					</td>
+					<td style="text-align:left; padding-left:10px;width:10px;padding-right:20px;">
+						<a style="color:#333;font-size:85%;line-height:1.5em;" href="www.google.com">
+							This
+							Day
+							in
+							History
+						</a>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="4">
+						&nbsp;
+					</td>
+				</tr>
+			</table>
+			<table width="600" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 600px; margin:0 auto;padding-top:35px; background-color:#FFF;">
+				<tr>
+					<td style="font-size:240%;font-weight:100;">Good Morning, Patrick.</td>
+				</tr>
+				<tr>
+					<td style="padding-top:20px">
+						<img src="<?=bloginfo('stylesheet_directory')?>/static/img/gmucf-feature.png" />
+						<table width="560" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 560px; margin:0 auto;padding-top:20px; background-color:#FFF;">
+							<tr>
+								<td colspan="2" style="padding-bottom:15px;">
+									<span style="font-size:190%;font-weight:100;">Harvey Runs on Water for 180 Yards</span>
+									<div style="padding-top:5px;font-weight:100;line-height:1.4em;padding-bottom:10px;font-size:100%">
+											Even while playing in a torrential rain Saturday night at Bright House Networks Stadium, UCF’s reign over Conference USA rival Marshall continued in full force. <a style="color:#9d1a1a;" href="www.google.com">Read More.</a>
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td  colspan="2" style="border-top:1px solid #ddd;border-bottom:1px solid #ddd;padding:3px 0;">
+									<table width="100%" border="0" align="center" cellpadding="0" bgcolor="#feedce" cellspacing="0" style="width: 100%;padding:20px 15px; background-color:#feedce;">
+										<tr>
+											<td>
+												<span style="display:block;margin:0;padding:0;color:#e84a13;font-size:100%;font-weight:bold;">WEATHER ADVISORY</span>
+												<p style="padding:10px 0 0 0;margin:0;font-weight:100;font-size:140%;line-height:1.4em;">
+													Severe weather expected today at UCF’s main campus. Please check local advisories before leaving for UCF. For more information visit today.ucf.edu.
+												</p>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr>
+								<td style="width:330px;padding-top:20px;">
+									<table width="100%" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width:100%; margin:0; background-color:#FFF;">
+										<tr>
+											<td style="border-bottom:1px solid #ddd;padding-bottom:20px;">
+												<table width="100%" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width:100%; margin:0; background-color:#FFF;">
+													<tr>
+														<td style="vertical-align:top;padding-bottom:40px;">
+															<img src="<?=bloginfo('stylesheet_directory')?>/static/img/gmucf-story1-feature.png" />
+														</td>
+														<td style="padding-left:20px;padding-bottom:40px;">
+															<span style="font-weight:bold;font-size:95%;line-height:1.3em;">
+																Growth: A Message from Dr. Hitt
+															</span>
+															<p style="font-family:Georgia,serif;font-size:85%;margin:3px 0 0 0;font-weight:100;line-height:1.4em;">
+																Dr. Marcy Verduin is the College of Medicine’s new associate dean for students. The search committee that interviewed candidates to replace retiring Associate Dean.
+															</p>
+														</td>
+													</tr>
+													<tr>
+														<td style="vertical-align:top;padding-bottom:40px;">
+															<img src="<?=bloginfo('stylesheet_directory')?>/static/img/gmucf-story2-feature.png" />
+														</td>
+														<td style="padding-left:20px;padding-bottom:40px;">
+															<span style="font-weight:bold;font-size:95%;line-height:1.3em;">
+																Robotics Club Brings Home 2nd Place from ASVC 2010
+															</span>
+															<p style="font-family:Georgia,serif;font-size:85%;margin:3px 0 0 0;font-weight:100;line-height:1.4em;">
+																The UCF Robotics Club brought home a $5,000 second place win from the third International Autonomous Surface.
+															</p>
+														</td>
+													</tr>
+													<tr>
+														<td style="vertical-align:top;padding-bottom:20px;">
+															<img src="<?=bloginfo('stylesheet_directory')?>/static/img/gmucf-story3-feature.png" />
+														</td>
+														<td style="padding-left:20px;padding-bottom:20px;">
+															<span style="font-weight:bold;font-size:95%;line-height:1.3em;">
+																Verduin Named College of Medicine’s Assoc Dean for Students
+															</span>
+															<p style="font-family:Georgia,serif;font-size:85%;margin:3px 0 0 0;font-weight:100;line-height:1.4em;">
+																Dr. Marcy Verduin is the College of Medicine’s new associate dean for students. The search committee that interviewed candidates to replace retiring Associate Dean Dr. Verduin’s appointment.
+															</p>
+														</td>
+													</tr>
+												</table>
+												<a style="font-weight:100;color:#9d1a1a;" href="www.google.com">More UCF Stories</a>
+											</td>
+										</tr>
+										<tr>
+											<td style="padding-top:40px;">
+												<span style="font-size:150%;font-weight:100;display:block;padding-bottom:40px;">
+													UCF Announcements
+												</span>
+												<p style="padding-bottom:20px;padding-top:0;margin:0;">
+													<a style="font-weight:100;color:#9d1a1a;" href="www.google.com">
+														Scholarship and Book Award Applications for USPS Employees
+													</a>
+												</p>
+												<p style="padding-bottom:20px;padding-top:0;margin:0;">
+													<a style="font-weight:100;color:#9d1a1a;" href="www.google.com">
+														Intermediate Microsoft Excel 2010 Course October 13th
+													</a>
+												</p>
+												<p style="padding-bottom:20px;padding-top:0;margin:0;">
+													<a style="font-weight:100;color:#9d1a1a;" href="www.google.com">
+														Intermediate Microsoft Access 2010 Course October 17th
+													</a>
+												</p>
+												<p style="padding-bottom:20px;padding-top:0;margin:0;">
+													<a style="font-weight:100;color:#9d1a1a;" href="www.google.com">
+														Fiber Optics Certification Courses
+													</a>
+												</p>
+											<td>
+										</tr>
+									</table>
+								</td>
+								<td style="width:230px;padding-top:20px;padding-left:20px;vertical-align:top;">
+									<table border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width:100%; margin:0; background-color:#FFF;">
+										<tr>
+											<td style="border-bottom:1px solid #ddd;padding-bottom:20px;padding-left:10px;">
+												<p style="font-size:150%;font-weight:100;margin-bottom:10px;">
+													Today @ UCF
+												</p>
+												<table border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width:100%; margin:0; background-color:#FFF;">
+												<? foreach(get_todays_events(array('limit'=>7)) as $event) {
+													$start_timestamp = strtotime($event->starts);
+													?>
+													<tr>
+														<td  style="text-align:center;background-color:#fef7c8;border:1px solid #e1e1e1;color:#666;padding:5px;">
+																	<span style="font-weight:bold;">
+																		<?=date('g:i', $start_timestamp)?>
+																	</span>
+																	<br />
+																	<?=date('A', $start_timestamp)?>
+														</td>
+												
+														<td style="padding-left:10px;">
+															<a href="<?=EVENTS_URL.'?eventdatetime_id='.$event->id?>" style="color:#333;font-weight:100;font-size:90%;">
+																<?=$event->title?>
+															</a>
+														</td>
+													</tr>
+													<tr>
+														<td colspan="2" style="height:10px;">&nbsp;</td>
+													</tr>
+												<? } ?>
+												</table>
+											</td>
+										</tr>
+										<tr>
+											<td style="padding-top:20px;padding-left:10px;">
+												<p style="font-size:150%;font-weight:100;margin-bottom:10px;">
+													Tomorrow @ UCF
+												</p>
+												<table width="100%" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width:100%; margin:0 0 20px 0; background-color:#FFF;">
+													<? foreach(get_tomorrows_events(array('limit'=>7)) as $event) { 
+														$start_timestamp = strtotime($event->starts);
+													?>
+													<tr>
+														<td  style="text-align:center;background-color:#fef7c8;border:1px solid #e1e1e1;color:#666;padding:5px;">
+																	<span style="font-weight:bold;">
+																		<?=date('g:i', $start_timestamp)?>
+																	</span>
+																	<br />
+																	<?=date('A', $start_timestamp)?>
+														</td>
+												
+														<td style="padding-left:10px;">
+															<a href="www.google.com" style="color:#333;font-weight:100;font-size:90%;">
+																<?=$event->title?>
+															</a>
+														</td>
+													</tr>
+													<tr>
+														<td colspan="2" style="height:10px;">&nbsp;</td>
+													</tr>
+												<? } ?>
+												</table>
+												<a style="font-weight:100;color:#9d1a1a;" href="<?=EVENTS_URL?>">
+													More Events
+												</a>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+						<table width="560" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 560px; margin:0 auto; background-color:#FFF;padding-top:65px;">
+							<tr>
+								<td style="width:330px;vertical-align:top;">
+									<table border="0" align="left" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="margin:0; background-color:#FFF;">
+										<tr>
+											<td colspan="3" style="font-size:125%;font-weight:100;padding-bottom:3px;">
+												Get Social:
+											</td>
+										</tr>
+										<tr>
+											<td>
+												<a href="http://www.google.com" style="text-decoration:none;">
+													<img style="border:0;" src="<?=bloginfo('stylesheet_directory')?>/static/img/gmucf-facebook.png" />
+												</a>
+											</td>
+											<td style="padding-left:10px;">
+												<a href="http://www.google.com" style="text-decoration:none;">
+													<img style="border:0;" src="<?=bloginfo('stylesheet_directory')?>/static/img/gmucf-youtube.png" />
+												</a>
+											</td>
+											<td style="padding-left:10px;">
+												<a href="http://www.google.com" style="text-decoration:none;">
+													<img style="border:0;" src="<?=bloginfo('stylesheet_directory')?>/static/img/gmucf-twitter.png" />
+												</a>
+											</td>
+										</tr>
+									</table>
+								</td>
+								<td style="width:230px;padding-left:40px;vertical-align:top;">
+									<a href="www.ucf.edu">
+										<img src="<?=bloginfo('stylesheet_directory')?>/static/img/gmucf-ucf-logo.png" style="border:0"/>
+									</a>
+									<p style="line-height:1.4em;font-size:90%;margin:0;padding:0;">
+										4000 Central Florida Blvd.
+										<br />
+										Orlando, FL 32816
+										<br />
+										407 823 2000
+									</p>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
