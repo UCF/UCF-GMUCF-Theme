41294ffb545d421f1c30b3b7ee390953b16ef725
Christopher Conover <clcono@gmail.com>
2011-12-02 11:21:36 -0500

    Made top story image, title and description links to the story

--- includes/top-story.php
+++ includes/top-story.php
@@ -7,14 +7,18 @@ if(!is_wp_error($rss)) {
 	$enclosure = $rss_item->get_enclosure();
 	$thumbnail = $enclosure->get_thumbnail();
 	?>
-	<img src="<?=$thumbnail?>" />
+	<a href="<?=$rss_item->get_permalink()?>">
+		<img style="border:none;" src="<?=$thumbnail?>" />
+	</a>
 	<table width="560" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 560px; margin:0 auto;padding-top:20px; background-color:#FFF;border-bottom:1px solid #ddd;">
 		<tr>
 			<td colspan="2" style="padding-bottom:15px;">
-				<span style="font-size:30px;font-weight:100;"><?=$rss_item->get_title()?></span>
-				<div style="padding-top:5px;font-weight:100;line-height:1.4em;padding-bottom:10px;font-size:18px">
-						<?=$rss_item->get_description()?> <a style="color:#9d1a1a;" href="<?=$rss_item->get_permalink()?>">Read More.</a>
-				</div>
+				<a href="<?=$rss_item->get_permalink()?>"  style="color:#333;text-decoration:none;">
+					<span style="font-size:30px;font-weight:100;"><?=$rss_item->get_title()?></span>
+					<div style="padding-top:5px;font-weight:100;line-height:1.4em;padding-bottom:10px;font-size:18px">
+							<?=$rss_item->get_description()?> <a style="color:#9d1a1a;" href="<?=$rss_item->get_permalink()?>">Read More.</a>
+					</div>
+				</a>
 			</td>
 		</tr>
 	</table>
