8e90996a5989ee594ee3b9a55366d284123fe179
Chris Conover <clcono@gmail.com>
2011-12-06 16:28:02 -0500

    Added alert read more link to template

--- includes/alert.php
+++ includes/alert.php
@@ -14,6 +14,11 @@ if(count($alerts) > 0) {
 					<p style="padding:10px 0 0 0;margin:0;font-weight:100;font-size:140%;line-height:1.4em;">
 						<?=esc_html($alert->post_content)?>
 					</p>
+					<? if( ($alert_uri = get_post_meta($alert->ID, 'alert_external_uri', True)) !== '') { ?>
+					<p style="margin:0;padding:20px 0 0 0;">
+						<a style="color:#333;font-size:140%;font-weight:100;" href="<?=esc_html($alert_uri)?>">Click here for more information</a>
+					</p>
+					<? } ?>
 				</td>
 			</tr>
 		</table>
