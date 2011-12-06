fdf581536447f386ea5c9e07aea447fa4aa5c65c
Chris Conover <clcono@gmail.com>
2011-12-06 16:19:42 -0500

    Escaped alert title and content html

--- includes/alert.php
+++ includes/alert.php
@@ -10,9 +10,9 @@ if(count($alerts) > 0) {
 		<table width="100%" border="0" align="center" cellpadding="0" bgcolor="#feedce" cellspacing="0" style="width: 100%;padding:20px 15px; background-color:#feedce;">
 			<tr>
 				<td>
-					<span style="display:block;margin:0;padding:0;color:#e84a13;font-size:100%;font-weight:bold;"><?=$alert->post_title?></span>
+					<span style="display:block;margin:0;padding:0;color:#e84a13;font-size:100%;font-weight:bold;"><?=esc_html($alert->post_title)?></span>
 					<p style="padding:10px 0 0 0;margin:0;font-weight:100;font-size:140%;line-height:1.4em;">
-						<?=$alert->post_content?>
+						<?=esc_html($alert->post_content)?>
 					</p>
 				</td>
 			</tr>
