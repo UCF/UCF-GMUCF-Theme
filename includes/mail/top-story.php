aa0f8b8922e148168c299bd7680ab20561147a62
Chris Conover <clcono@gmail.com>
2011-12-21 10:20:24 -0500

    Top story image, title and copy are now links to the story

--- includes/mail/top-story.php
+++ includes/mail/top-story.php
@@ -1,13 +1,24 @@
 <?php
 extract(get_top_story_details());
 ?>
-<img style="border:none;" src="<?=$thumbnail_src?>" />
+<a href="<?=$read_more_uri?>">
+	<img style="border:none;" src="<?=$thumbnail_src?>" />
+</a>
 <table width="560" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 560px; margin:0 auto;padding-top:20px; background-color:#FFF;border-bottom:1px solid #ddd;">
 	<tr>
 		<td colspan="2" style="padding-bottom:15px;">
-			<span style="font-size:30px;font-weight:100;"><?=$story_title?></span>
+			<a href="<?=$read_more_uri?>">
+				<span style="font-size:30px;font-weight:100;"><?=$story_title?></span>
+			</a>
 			<div style="padding-top:5px;font-weight:100;line-height:1.4em;padding-bottom:10px;font-size:18px">
-					<?=$story_description?> <? if($read_more_uri != '') {?><a style="color:#9d1a1a;text-decoration:underline;" href="<?=$read_more_uri?>">Read More.</a><?}?>
+					<a href="<?=$read_more_uri?>">
+						<?=$story_description?>
+					</a>
+					<? if($read_more_uri != '') {?>
+						<a style="color:#9d1a1a;text-decoration:underline;" href="<?=$read_more_uri?>">
+							Read More.
+						</a>
+					<?}?>
 			</div>
 		</td>
 	</tr>
