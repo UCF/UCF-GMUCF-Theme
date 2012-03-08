d1b174dde2127797d4793913e0d89cf301835925
Christopher Conover <clcono@gmail.com>
2012-03-08 11:59:08 -0500

    Finished styling extended weather

--- includes/events/mail/base.php
+++ includes/events/mail/base.php
@@ -65,106 +65,115 @@ switch($edition) {
 				</td>
 			</tr>
 			<tr>
-				<? $weather = get_extended_weather(); ?>
-				<table width="600" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 600px; margin:0; background-color:#FFF;">
+				<td>
+					<div class="clear">&nbsp;</div>
+					<? $weather = get_extended_weather(); ?>
+					<table width="600" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 600px; margin:0; background-color:#FFF;">
 						<tr>
-							<td style="width:120px;">
-								<table width="120" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 120px; margin:0; background-color:#FFF;">
+							<td style="width:110px;border-right:1px solid #ddd;padding-left:10px;">
+								<table width="110" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 110px; margin:0; background-color:#FFF;">
 									<tr>
 										<td style="width:60px">
-											<span>TODAY</span>
+											<span style="font-size:10px;font-weight:bold;">TODAY</span>
 											<img src="<?=bloginfo('stylesheet_directory')?>/static/img/weather/<?=$weather[0]['image']?>.png" />
 										</td>
 										<td>
-											<?=$weather[0]['high']?>&deg;
+											<span style="font-size:18px;font-weight:bold"><?=$weather[0]['high']?>&deg;</span>
 											<br />
-											High
+											<span style="font-size:12px;">High</span>
 											<br />
-											<?=$weather[0]['low']?>&deg;
+											<span style="font-size:18px;font-weight:bold"><?=$weather[0]['low']?>&deg;</span>
 											<br />
-											Low
+											<span style="font-size:12px;">Low</span>
 										</td>
 									</tr>
 								</table>
 							</td>
-							<td style="width:120px;">
-								<table width="120" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 120px; margin:0; background-color:#FFF;">
+							<td style="width:110px;border-right:1px solid #ddd;padding-left:10px;">
+								<table width="110" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 110px; margin:0; background-color:#FFF;">
 									<tr>
 										<td style="width:60px">
-											<span>TOMORROW</span>
+											<span style="font-size:10px;font-weight:bold;">TOMORROW</span>
 											<img src="<?=bloginfo('stylesheet_directory')?>/static/img/weather/<?=$weather[1]['image']?>.png" />
 										</td>
 										<td>
-											<?=$weather[1]['high']?>&deg;
+											<span style="font-size:18px;font-weight:bold"><?=$weather[1]['high']?>&deg;</span>
 											<br />
-											High
+											<span style="font-size:12px;">High</span>
 											<br />
-											<?=$weather[1]['low']?>&deg;
+											<span style="font-size:18px;font-weight:bold"><?=$weather[1]['low']?>&deg;</span>
 											<br />
-											Low
+											<span style="font-size:12px;">Low</span>
 										</td>
 									</tr>
 								</table>
 							</td>
-							<td style="width:120px;">
-								<table width="120" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 120px; margin:0; background-color:#FFF;">
+							<td style="width:110px;border-right:1px solid #ddd;padding-left:10px;">
+								<table width="110" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 110px; margin:0; background-color:#FFF;">
 									<tr>
 										<td style="width:60px">
-											<span>WEDNESDAY</span>
+											<span style="font-size:10px;font-weight:bold;">WEDNESDAY</span>
 											<img src="<?=bloginfo('stylesheet_directory')?>/static/img/weather/<?=$weather[2]['image']?>.png" />
 										</td>
 										<td>
-											<?=$weather[2]['high']?>&deg;
+											<span style="font-size:18px;font-weight:bold"><?=$weather[2]['high']?>&deg;</span>
 											<br />
-											High
+											<span style="font-size:12px;">High</span>
 											<br />
-											<?=$weather[2]['low']?>&deg;
+											<span style="font-size:18px;font-weight:bold"><?=$weather[2]['low']?>&deg;</span>
 											<br />
-											Low
+											<span style="font-size:12px;">Low</span>
 										</td>
 									</tr>
 								</table>
 							</td>
-							<td style="width:120px;">
-								<table width="120" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 120px; margin:0; background-color:#FFF;">
+							<td style="width:110px;border-right:1px solid #ddd;padding-left:10px;">
+								<table width="110" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 110px; margin:0; background-color:#FFF;">
 									<tr>
 										<td style="width:60px">
-											<span>THURSDAY</span>
+											<span style="font-size:10px;font-weight:bold;">THURSDAY</span>
 											<img src="<?=bloginfo('stylesheet_directory')?>/static/img/weather/<?=$weather[3]['image']?>.png" />
 										</td>
 										<td>
-											<?=$weather[3]['high']?>&deg;
+											<span style="font-size:18px;font-weight:bold"><?=$weather[3]['high']?>&deg;</span>
 											<br />
-											High
+											<span style="font-size:12px;">High</span>
 											<br />
-											<?=$weather[3]['low']?>&deg;
+											<span style="font-size:18px;font-weight:bold"><?=$weather[3]['low']?>&deg;</span>
 											<br />
-											Low
+											<span style="font-size:12px;">Low</span>
 										</td>
 									</tr>
 								</table>
 							</td>
-							<td style="width:120px;">
-								<table width="120" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 120px; margin:0; background-color:#FFF;">
+							<td style="width:110px;padding-left:10px;">
+								<table width="110" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width: 110px; margin:0; background-color:#FFF;">
 									<tr>
 										<td style="width:60px">
-											<span>FRIDAY</span>
+											<span style="font-size:10px;font-weight:bold;">FRIDAY</span>
 											<img src="<?=bloginfo('stylesheet_directory')?>/static/img/weather/<?=$weather[4]['image']?>.png" />
 										</td>
 										<td>
-											<?=$weather[4]['high']?>&deg;
+											<span style="font-size:18px;font-weight:bold"><?=$weather[4]['high']?>&deg;</span>
 											<br />
-											High
+											<span style="font-size:12px;">High</span>
 											<br />
-											<?=$weather[4]['low']?>&deg;
+											<span style="font-size:18px;font-weight:bold"><?=$weather[4]['low']?>&deg;</span>
 											<br />
-											Low
+											<span style="font-size:12px;">Low</span>
 										</td>
 									</tr>
 								</table>
 							</td>
 						</tr>
 					</table>
+					<div class="clear">&nbsp;</div>
+				</td>
+			</tr>
+			<tr>
+				<td style="border-top:1px solid #ddd;padding-top:35px;">
+					aaa
+				</td>
 			</tr>
 		</table>
 	</body>
