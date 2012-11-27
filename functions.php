9329d7ccf965b9bfa65545ffdff25e64c3d5696f
Chris Conover <clcono@gmail.com>
2012-11-27 10:31:13 -0500

    Convenience template redirects

--- functions.php
+++ functions.php
@@ -831,4 +831,65 @@ function get_theme_option($name) {
 	}
 }
 
+/**
+ * Convenience template redirects 
+ *
+ * @author Chris Conover
+ **/
+function gmucf_template_redirect() {
+
+	function display_gmucf_template($template) {
+		header("HTTP/1.1 200 OK");
+		get_template_part($template);
+		exit;
+	}
+
+	# Get the path without server host or leading blog stuff (e.g. /wp3/gmucf)
+	$parts = explode($_SERVER['SERVER_NAME'], home_url('/'));
+	if(count($parts) == 2) {
+		$path = str_replace($parts[1], '', $_SERVER['REQUEST_URI']);
+		switch($path) {
+			# News
+			case 'news/':
+				display_gmucf_template('includes/news/browser/base');
+				break;
+			case 'news/mail/':
+				display_gmucf_template('includes/news/mail/base');
+				break;
+			case 'news/text/':
+				display_gmucf_template('includes/news/text/base');
+				break;
+			# Events
+			## Weekday
+			case 'events/weekday/':
+				$_GET['edition'] = 'weekday';
+				display_gmucf_template('includes/events/browser/base');
+				break;
+			case 'events/weekday/mail/':
+				$_GET['edition'] = 'weekday';
+				display_gmucf_template('includes/events/mail/base');
+				break;
+			case 'events/weekday/text/':
+				$_GET['edition'] = 'weekday';
+				display_gmucf_template('includes/events/text/base');
+				break;
+			## Weekend
+			case 'events/weekend/':
+				$_GET['edition'] = 'weekend';
+				display_gmucf_template('includes/events/browser/base');
+				break;
+			case 'events/weekend/mail/':
+				$_GET['edition'] = 'weekend';
+				display_gmucf_template('includes/events/mail/base');
+				break;
+			case 'events/weekend/text/':
+				$_GET['edition'] = 'weekend';
+				display_gmucf_template('includes/events/text/base');
+				break;
+		}
+
+		
+	}
+}
+add_action('template_redirect', 'gmucf_template_redirect', 1);
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
-
-if(isset($_GET['events'])) {
-	if(isset($_GET['accessible'])) {
-		get_template_part('includes/events/browser/base');
-	} else if(isset($_GET['text'])) {
-		get_template_part('includes/events/text/base');
-	} else {
-		get_template_part('includes/events/mail/base');
-	}
-} else {
-	if(isset($_GET['accessible'])) {
-		get_template_part('includes/news/browser/base');
-	} else if(isset($_GET['text'])) {
-		get_template_part('includes/news/text/base');
-	} else {
-		get_template_part('includes/news/mail/base');
-	}
-}
-
+	disallow_direct_load('page.php');
+	get_template_part('includes/news/browser/base');
 ?>
