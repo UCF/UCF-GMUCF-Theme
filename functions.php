81bca3793f09d293a018524672ffbdb3e0da06d3
Chris Conover <clcono@gmail.com>
2012-11-27 10:53:11 -0500

    Allow GET parameters to be passed

--- functions.php
+++ functions.php
@@ -847,48 +847,25 @@ function gmucf_template_redirect() {
 	# Get the path without server host or leading blog stuff (e.g. /wp3/gmucf)
 	$parts = explode($_SERVER['SERVER_NAME'], home_url('/'));
 	if(count($parts) == 2) {
-		$path = str_replace($parts[1], '', $_SERVER['REQUEST_URI']);
-		switch($path) {
-			# News
-			case 'news/':
-				display_gmucf_template('includes/news/browser/base');
-				break;
-			case 'news/mail/':
-				display_gmucf_template('includes/news/mail/base');
-				break;
-			case 'news/text/':
-				display_gmucf_template('includes/news/text/base');
-				break;
-			# Events
-			## Weekday
-			case 'events/weekday/':
-				$_GET['edition'] = 'weekday';
-				display_gmucf_template('includes/events/browser/base');
-				break;
-			case 'events/weekday/mail/':
-				$_GET['edition'] = 'weekday';
-				display_gmucf_template('includes/events/mail/base');
-				break;
-			case 'events/weekday/text/':
-				$_GET['edition'] = 'weekday';
-				display_gmucf_template('includes/events/text/base');
-				break;
-			## Weekend
-			case 'events/weekend/':
-				$_GET['edition'] = 'weekend';
-				display_gmucf_template('includes/events/browser/base');
-				break;
-			case 'events/weekend/mail/':
-				$_GET['edition'] = 'weekend';
-				display_gmucf_template('includes/events/mail/base');
-				break;
-			case 'events/weekend/text/':
-				$_GET['edition'] = 'weekend';
-				display_gmucf_template('includes/events/text/base');
-				break;
-		}
+		$request_path = str_replace($parts[1], '', $_SERVER['REQUEST_URI']);
 
-		
+		# Most to least specific
+		$mapping = array(
+			'news/mail/'           => create_function('', 'display_gmucf_template(\'includes/news/mail/base\');'),
+			'news/text/'           => create_function('', 'display_gmucf_template(\'includes/news/text/base\');'),
+			'news/'                => create_function('', 'display_gmucf_template(\'includes/news/browser/base\');'),
+			'events/weekday/mail/' => create_function('', '$_GET[\'edition\'] = \'weekday\';display_gmucf_template(\'includes/events/mail/base\');'),
+			'events/weekday/text/' => create_function('', '$_GET[\'edition\'] = \'weekday\';display_gmucf_template(\'includes/events/text/base\');'),
+			'events/weekday/'      => create_function('', '$_GET[\'edition\'] = \'weekday\';display_gmucf_template(\'includes/events/browser/base\');'),
+			'events/weekend/mail/' => create_function('', '$_GET[\'edition\'] = \'weekend\';display_gmucf_template(\'includes/events/mail/base\');'),
+			'events/weekend/text/' => create_function('', '$_GET[\'edition\'] = \'weekend\';display_gmucf_template(\'includes/events/text/base\');'),
+			'events/weekend/'      => create_function('', '$_GET[\'edition\'] = \'weekend\';display_gmucf_template(\'includes/events/browser/base\');'));
+
+		foreach($mapping as $path => $func) {
+			if(stripos($request_path, $path) == 0) {
+				call_user_func($func);
+			}
+		}
 	}
 }
 add_action('template_redirect', 'gmucf_template_redirect', 1);
