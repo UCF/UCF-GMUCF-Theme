0d0cc267d7aa7f4304814ca5247bf4605b98beaf
Chris Conover <clcono@gmail.com>
2012-11-29 13:11:20 -0500

    Path fix

--- functions.php
+++ functions.php
@@ -849,7 +849,11 @@ function gmucf_template_redirect() {
 	# Get the path without server host or leading blog stuff (e.g. /wp3/gmucf)
 	$parts = explode($_SERVER['SERVER_NAME'], home_url('/'));
 	if(count($parts) == 2) {
-		$request_path = str_replace($parts[1], '', $_SERVER['REQUEST_URI']);
+		if($parts[1] != '/') {
+			$request_path = str_replace($parts[1], '', $_SERVER['REQUEST_URI']);
+		} else {
+			$request_path = substr($_SERVER['REQUEST_URI'], 1);
+		}
 
 		# Most to least specific
 		$mapping = array(
