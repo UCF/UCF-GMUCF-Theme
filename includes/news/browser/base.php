0af2d72a34831aadac69fdf2a5dd1ef0e5460421
Jo Dickson <jogreybill@gmail.com>
2016-11-14 11:38:28 -0600

    Removed all wotd code

--- includes/news/browser/base.php
+++ includes/news/browser/base.php
@@ -5,35 +5,6 @@
 	<hr />
 	<?=get_template_part('includes/news/browser/alert')?>
 	<?=get_template_part('includes/news/browser/featured-stories')?>
-	<? $wotd = get_word_of_the_day(); ?>
-	<? if($wotd !== False) { ?>
-	<hr />
-	<div id="wotd" style="clear:both;">
-		<h2>Word of the Day</h2>
-		<span id="word"><b><?=$wotd['word']?></b> <strong>-</strong> \<?=$wotd['pronunciation']?>\ <strong>-</strong> <em><?=$wotd['partofspeech']?></em></span>
-		<br />
-		<? foreach($wotd['definitions'] as $part=>$definitions) { ?>
-			<? if($part != $wotd['partofspeech']) { ?>
-				<p><em><?=$part?></em></p>
-			<? } ?>
-			<? $count = 1; ?>
-			<? foreach($definitions as $definition) { ?>
-				<p><?=$count?>. <?=$definition?></p>
-				<? $count++;?>
-			<? } ?>
-		<? } ?>
-		<? if(count($wotd['examples']) > 0) { ?>
-			<? foreach($wotd['examples'] as $example) { ?>
-			<p class="quote">&ldquo;<?=$example['quote']?>&rdquo;<br />&mdash;<?=$example['source']?>, <?=$example['author']?></p>
-			<? } ?>
-		<? } ?>
-		<div id="credit-link">
-			<a href="http://www.dictionary.com" class="ignore-external">
-				<img src="<?=bloginfo('stylesheet_directory')?>/static/img/dictionary.com-attribution.png"/>
-			</a>
-		</div>
-	</div>
-	<? } ?>
 	<hr />
 	<?=get_template_part('includes/news/browser/announcements')?>
 	<div id="bottom" class="clearfix">
--- includes/news/text/base.php
+++ includes/news/text/base.php
@@ -11,26 +11,12 @@ Tonight's Low: <?=$weather['tonight']['tempN']?><? $top_story_details = get_top_
 
 <?= strip_tags($top_story_details['story_description']) ?>
 
-<? foreach(get_featured_stories_details() as $detail) {?> 
+<? foreach(get_featured_stories_details() as $detail) {?>
 - <?= strip_tags($detail['title']) ?>
 
 <?= strip_tags($detail['description']) ?>
 
 <? } ?>
-
-<? $wotd = get_word_of_the_day(); ?>
-<? if($wotd !== False) { ?>
--- Word of the Day, Provided by dictionary.com
-
-<?=$wotd['word']?> - \<?=$wotd['pronunciation']?>\ - <?=$wotd['partofspeech']?>
-
-<? foreach($wotd['definitions'] as $part=>$definitions) { ?>
-<? if($part != $wotd['partofspeech']) { ?><?=$part?>
-<? } ?><? $count = 1; ?>
-<? foreach($definitions as $definition) { ?><?=$count?>. <?=$definition?><? $count++;?><? } ?>
-<? } ?>
-<? } ?>
-
 
 -- Announcements
 
@@ -40,7 +26,7 @@ Tonight's Low: <?=$weather['tonight']['tempN']?><? $top_story_details = get_top_
 <? } ?>
 
 
-<?php 
+<?php
 # Because of insanity, the fetch_feed function changes the Content-Type header
 # somehow. I didn't bother tracking it down because I don't care. Just force
 # the Content-Type down here instead of at the top.
