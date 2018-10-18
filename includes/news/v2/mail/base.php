ba23ac5cac4a227a8e2fa5795ddd446019c2c608
Cadie Brown <cadie.brown@gmail.com>
2018-10-18 15:38:46 -0400

    Put title back on one line

--- includes/news/v2/mail/base.php
+++ includes/news/v2/mail/base.php
@@ -4,10 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="format-detection" content="telephone=no">
-	<title>Good
-		<?php echo (int)date('G') >= 12 ? 'Afternoon' : 'Morning'; ?> UCF -
-		<?php echo date('F j'); ?>
-	</title>
+	<title>Good <?php echo (int)date('G') >= 12 ? 'Afternoon' : 'Morning'; ?> UCF - <?php echo date('F j'); ?></title>
 	<?php echo get_template_part('includes/news/v2/mail/style'); ?>
 </head>
 
