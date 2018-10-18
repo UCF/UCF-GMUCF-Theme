7ee028966970a2834d4a76c347b7cec940ccdb85
RJ Bruneel <rj.bruneel@ucf.edu>
2018-10-18 14:07:25 -0400

    Moved logic to v2 templates

--- includes/news/v2/mail/announcements.php
+++ includes/news/v2/mail/announcements.php
@@ -1,1 +1,18 @@
 <h2>Announcements</h1>
+
+<ul>
+<?php
+foreach(get_announcement_details() as $announcement) :
+	extract($announcement);
+?>
+	<li>
+		<a href="<?=$permalink?>">
+			<?=$title?>
+		</a>
+	</li>
+<?php endforeach; ?>
+</ul>
+
+<a href="<?=ANNOUNCEMENTS_MORE_URL?>">
+	More Announcements
+</a>
