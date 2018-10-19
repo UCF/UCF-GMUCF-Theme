e91fd5f3f767a79c59d0564be18a4e91b2961a9a
cadie <cadie.brown@gmail.com>
2018-10-19 00:58:23 -0400

    Update Header

--- includes/news/v2/mail/header.php
+++ includes/news/v2/mail/header.php
@@ -1,15 +1,17 @@
-<h1>UCF Today</h1>
-<h2>Good <?php echo (int)date('G') >= 12 ? 'Afternoon' : 'Morning'; ?>, !@!Preferred Name!@!.</h2>
-<?php
-echo date('l, F j, Y');
+<tr>
+	<td style="padding-top: 0; padding-bottom: 15px; padding-left: 0; padding-right: 0; text-align: center;">
+		<a href="https://today.ucf.edu"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/logo-today.jpg" alt="UCF Today" border="0" width="" height=""></a>
+	</td>
+</tr>
 
-$weather = get_weather( 'weather-today' );
+<tr>
+	<td class="montserratsemibold" style="font-family: Helvetica, Arial, sans-serif; font-size: 14px; font-weight: bold; padding-left: 0; padding-right: 0; padding-top: 0; color: #848484; text-align: center; padding-bottom: 0; line-height: 1; text-transform: uppercase; letter-spacing: 1.25px;">
+		<?php echo date('l, F j, Y'); ?>
+	</td>
+</tr>
 
-if (!empty($weather)) :
-?>
-	<img height="36" width="36" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $weather['today']['imgCode']; ?>.png" width="30" />
-	High <strong><?php echo $weather['today']['tempN']; ?>&deg;</strong>
-
-	<img height="36" width="36" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $weather['tonight']['imgCode']; ?>.png" width="30" />
-	Low <strong><?php echo $weather['tonight']['tempN']; ?>&deg;</strong>
-<?php endif; ?>
+<tr>
+	<td class="montserratlight" style="font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: bold; padding-bottom: 30px; padding-top: 30px; padding-left: 0; padding-right: 0; text-align: left;" align="left">
+		<h2 style="margin-top: 0; margin-bottom: 0; font-weight: normal;">Good <?php echo (int)date('G') >= 12 ? 'Afternoon' : 'Morning'; ?>, !@!Preferred Name!@!.</h2>
+	</td>
+</tr>
