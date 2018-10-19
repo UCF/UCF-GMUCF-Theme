ab0f49f0b79d6b43fa60050ca9ff5fa6372729c9
cadie <cadie.brown@gmail.com>
2018-10-19 02:51:30 -0400

    Adjust Announcements list styles

--- includes/news/v2/mail/announcements.php
+++ includes/news/v2/mail/announcements.php
@@ -1,5 +1,5 @@
 <tr>
-  <td class="montserratbold" style="padding-top: 20px; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: bold; color: #000; text-transform: uppercase;" align="left">
+  <td class="montserratbold" style="padding-top: 20px; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 20px; font-weight: bold; color: #000; text-transform: uppercase;" align="left">
 	<a style="text-decoration: none; color: #000;" href="https://www.ucf.edu/announcements/">Announcements</a>
 	<div style="width: 35px; border-top: 3px solid #fc0; margin-top: 5px;"></div>
   </td>
@@ -15,12 +15,12 @@
 		<?php
 		} else {
 			?>
-			<ul>
+			<ul style="list-style: none; padding-left: 0;">
 			<?php
 			foreach( $announcements as $announcement ) :
 				extract( $announcement );
 			?>
-				<li>
+				<li class="montserratlight" style="padding-top: 8px; padding-bottom: 8px;font-family: Helvetica, Arial, sans-serif; color: #000;" align="left">
 					<a href="<?php echo $permalink; ?>">
 						<?php echo $title;?>
 					</a>
