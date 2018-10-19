debf85e259c40a9e5663901b011883fb7ece29ed
cadie <cadie.brown@gmail.com>
2018-10-18 23:53:23 -0400

    Update Footer to match new layout

--- includes/news/v2/mail/footer.php
+++ includes/news/v2/mail/footer.php
@@ -1,84 +1,58 @@
+<tr>
+	<td style="padding-bottom: 20px; padding-top: 40px; padding-left: 0; padding-right: 0; text-align: center" align="center">
+		<a href="https://www.instagram.com/ucf.edu">
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/instagram.png" alt="Instagram" align="center" class="float-center"
+				alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="32" height="32"></a>
+		<a href="https://www.twitter.com/UCF/">
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/twitter.png" alt="Twitter" align="center" class="float-center"
+				alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="32" height="32"></a>
+		<a href="https://www.facebook.com/ucf/">
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/facebook.png" alt="Facebook" align="center" class="float-center"
+				alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="32" height="32"></a>
+		<a href="https://www.youtube.com/user/UCF/">
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/youtube.png" alt="YouTube" align="center" class="float-center"
+				alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="32" height="32"></a>
+	</td>
+</tr>
 
-<table class="row">
-	<tbody>
-		<tr>
-			<th class="small-12 large-12 columns first last">
-				<hr>
-			</th>
-		</tr>
-	</tbody>
-</table>
+<tr>
+	<td style="padding-bottom: 20px; padding-top: 20px; padding-left: 0; padding-right: 0; text-align: center" align="center">
+			<a href="https://www.ucf.edu"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/ucf-tab.png" width="100" height="135" alt="UCF Tab" align="center" class="float-center"></a>
+	</td>
+</tr>
 
-<table class="row">
-	<tbody>
-		<tr>
-			<th class="small-12 large-12 columns first last">
-			    <center>
-					<a href="https://www.instagram.com/ucf.edu">
-						<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/instagram.png" alt="Instagram" align="center" class="float-center"
-							alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="32" height="32"></a>
-					<a href="https://www.twitter.com/UCF/">
-						<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/twitter.png" alt="Twitter" align="center" class="float-center"
-							alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="32" height="32"></a>
-					<a href="https://www.facebook.com/ucf/">
-						<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/facebook.png" alt="Facebook" align="center" class="float-center"
-							alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="32" height="32"></a>
-					<a href="https://www.youtube.com/user/UCF/">
-						<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/youtube.png" alt="YouTube" align="center" class="float-center"
-							alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="32" height="32"></a>
-				</center>
-			</th>
-		</tr>
-	</tbody>
-</table>
+<tr>
+  <td class="montserratbold" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: bold; text-transform: uppercase; color: #000; letter-spacing: 1.25px;" align="center">
+    <a style="text-decoration: none; color: #000;" href="https://www.ucf.edu/">University of Central Florida</a>
+  </td>
+</tr>
 
+<tr>
+  <td class="montserratlight" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+    4000 Central Florida Blvd.
+  </td>
+</tr>
 
-<table class="row">
-	<tbody>
-		<tr>
-			<th class="small-12 large-12 columns first last float-center" align="center">
-				<center>
-					<a href="https://www.ucf.edu"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/ucf-tab.png" width="150" height="202" alt="UCF Tab" align="center" class="float-center"></a>
-				</center>
-			</th>
-		</tr>
-	</tbody>
-</table>
+<tr>
+  <td class="montserratlight" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+    Orlando, FL 32816
+  </td>
+</tr>
 
-<table class="row">
-	<tbody>
-		<tr>
-			<th class="small-12 large-12 columns first last float-center" align="center">
-				<center>
-					<strong>University of Central Florida</strong><br>
-					4000 Central Florida Blvd.<br>
-					Orlando, FL 32816<br>
-					407-823-2000<br>
-				</center>
-			</th>
-		</tr>
-	</tbody>
-</table>
+<tr>
+  <td class="montserratlight" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+    407-823-2000
+  </td>
+</tr>
 
-<table class="row">
-	<tbody>
-		<tr>
-			<th class="small-12 large-12 columns first last float-center" align="center">
-				<center>
-					<a href="#">Suggest a Story</a> | <a href="#">Tell Us What You Think</a>
-				</center>
-			</th>
-		</tr>
-	</tbody>
-</table>
+<tr>
+	<td class="montserratbold" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: bold; color: #000;" align="center">
+		<a href="#">Suggest a Story</a> | <a href="#">Tell Us What You Think</a>
+	</td>
+</tr>
 
-<table class="row">
-	<tbody>
-		<tr>
-			<th class="small-12 large-12 columns first last float-center" align="center">
-				<center>
-					!@!UNSUBSCRIBE!@! from this email.
-				</center>
-			</th>
-		</tr>
-</table>
+<tr>
+	<td class="montserratlight" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+		!@!UNSUBSCRIBE!@! from this email.
+	</td>
+</tr>
