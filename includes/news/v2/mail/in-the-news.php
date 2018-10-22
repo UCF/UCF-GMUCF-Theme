7f4d53c5c2ec5e974c66d0b2ff8be18d61f22248
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-10-22 16:07:07 -0400

    Removed anchor around heading for in the news

--- includes/news/v2/mail/in-the-news.php
+++ includes/news/v2/mail/in-the-news.php
@@ -1,6 +1,6 @@
 <tr>
   <td class="montserratbold" style="padding-top: 20px; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 20px; font-weight: bold; color: #000; text-transform: uppercase;" align="left">
-	<a style="text-decoration: none; color: #000;" href="https://www.ucf.edu/announcements/">UCF In the News</a>
+	<a style="text-decoration: none; color: #000;">UCF In the News</a>
 	<div style="width: 35px; border-top: 3px solid #fc0; margin-top: 5px;"></div>
   </td>
 </tr>
