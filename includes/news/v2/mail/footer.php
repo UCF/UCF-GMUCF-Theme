1377b5d60ab334306311b2da10ee7701549ffe34
Cadie Brown <cadie.brown@gmail.com>
2018-10-24 15:05:20 -0400

    Tabbing fixes

--- includes/news/v2/mail/footer.php
+++ includes/news/v2/mail/footer.php
@@ -1,20 +1,15 @@
 <tr>
 	<td style="padding-bottom: 20px; padding-top: 40px; padding-left: 0; padding-right: 0; text-align: center;" align="center">
 		<a href="https://www.facebook.com/ucf/">
-			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/facebook.png" alt="Facebook" align="center" class="float-center"
-				alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="42" height="42"></a>
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/facebook.png" alt="Facebook" align="center" class="float-center" alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="42" height="42"></a>
 		<a href="https://www.twitter.com/UCF/">
-			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/twitter.png" alt="Twitter" align="center" class="float-center"
-				alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="42" height="42"></a>
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/twitter.png" alt="Twitter" align="center" class="float-center" alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="42" height="42"></a>
 		<a href="https://www.instagram.com/ucf.edu">
-			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/instagram.png" alt="Instagram" align="center" class="float-center"
-				alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="42" height="42"></a>
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/instagram.png" alt="Instagram" align="center" class="float-center" alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="42" height="42"></a>
 		<a href="https://www.linkedin.com/edu/university-of-central-florida-18118">
-			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/linkedin.png" alt="LinkedIn" align="center" class="float-center"
-				alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="42" height="42"></a>
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/linkedin.png" alt="LinkedIn" align="center" class="float-center" alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="42" height="42"></a>
 		<a href="https://www.youtube.com/user/UCF/">
-			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/youtube.png" alt="YouTube" align="center" class="float-center"
-				alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="42" height="42"></a>
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/youtube.png" alt="YouTube" align="center" class="float-center" alt="UCF Tab" align="center" class="float-center" style="display:inline-block" width="42" height="42"></a>
 	</td>
 </tr>
 
@@ -25,27 +20,27 @@
 </tr>
 
 <tr>
-  <td class="montserratbold" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: bold; text-transform: uppercase; color: #000; letter-spacing: 1.25px;" align="center">
-    <a style="text-decoration: none; color: #000;" href="https://www.ucf.edu/">University of Central Florida</a>
-  </td>
+	<td class="montserratbold" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: bold; text-transform: uppercase; color: #000; letter-spacing: 1.25px;" align="center">
+		<a style="text-decoration: none; color: #000;" href="https://www.ucf.edu/">University of Central Florida</a>
+	</td>
 </tr>
 
 <tr>
-  <td class="montserratlight" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
-    4000 Central Florida Blvd.
-  </td>
+	<td class="montserratlight" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+		4000 Central Florida Blvd.
+	</td>
 </tr>
 
 <tr>
-  <td class="montserratlight" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
-    Orlando, FL 32816
-  </td>
+	<td class="montserratlight" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+		Orlando, FL 32816
+	</td>
 </tr>
 
 <tr>
-  <td class="montserratlight" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
-    407-823-2000
-  </td>
+	<td class="montserratlight" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+		407-823-2000
+	</td>
 </tr>
 
 <tr>
