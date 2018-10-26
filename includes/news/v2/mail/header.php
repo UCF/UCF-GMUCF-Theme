35ce7e28607f127d6010c4348ca7943b0b975947
RJ Bruneel <rj.bruneel@ucf.edu>
2018-10-26 15:42:37 -0400

    Design updates and updated the base.php for consistancy.

--- includes/news/v2/mail/header.php
+++ includes/news/v2/mail/header.php
@@ -1,31 +1,33 @@
-<tr>
-	<td style="padding-top: 0; padding-bottom: 20px; padding-left: 0; padding-right: 0; text-align: center; border-bottom: 3px solid #fc0;">
-		<a href="https://today.ucf.edu"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/logo-today.jpg" alt="UCF Today" border="0" width="" height=""></a>
-	</td>
-</tr>
-
-<tr>
-	<td class="montserratlight" style="font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: bold; padding-bottom: 30px; padding-top: 20px; padding-left: 0; padding-right: 0; text-align: center;" align="center">
-		<h2 style="margin-top: 0; margin-bottom: 0; font-weight: normal;">Good <?php echo (int)date('G') >= 12 ? 'Afternoon' : 'Morning'; ?>, !@!Preferred Name!@!.</h2>
-	</td>
-</tr>
-
-<tr>
-	<td class="montserratsemibold" style="font-family: Helvetica, Arial, sans-serif; font-size: 14px; font-weight: bold; padding-left: 0; padding-right: 0; padding-top: 0; color: #848484; text-align: center; padding-bottom: 15px; line-height: 1; text-transform: uppercase; letter-spacing: 1.25px;">
-		<?php echo date('l, F j, Y'); ?>
-	</td>
-</tr>
-
-<?php
-$weather = get_weather( 'weather-today' );
-if (!empty($weather)) :
-?>
+<table width="100%" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width:100%; margin:0; background-color:#fff;">
 	<tr>
-		<td style="font-family: Helvetica, Arial, sans-serif; font-size: 14px; padding-left: 0; padding-right: 0; padding-top: 0; color: #848484; text-align: center; padding-bottom: 0; line-height: 1;" align="center">
-			<img height="32" width="32" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $weather['today']['imgCode']; ?>.png" />
-			High <strong><?php echo $weather['today']['tempN']; ?>&deg;</strong>
-			<img height="32" width="32" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $weather['tonight']['imgCode']; ?>.png" />
-			Low <strong><?php echo $weather['tonight']['tempN']; ?>&deg;</strong>
+		<td style="padding-top: 0; padding-bottom: 20px; padding-left: 0; padding-right: 0; text-align: center; border-bottom: 3px solid #fc0;">
+			<a href="https://today.ucf.edu"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/logo-today.jpg" alt="UCF Today" border="0" width="" height=""></a>
 		</td>
 	</tr>
-<?php endif; ?>
+
+	<tr>
+		<td class="montserratlight" style="font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: bold; padding-bottom: 30px; padding-top: 20px; padding-left: 0; padding-right: 0; text-align: center;" align="center">
+			<h2 style="margin-top: 0; margin-bottom: 0; font-weight: normal;">Good <?php echo (int)date('G') >= 12 ? 'Afternoon' : 'Morning'; ?>, !@!Preferred Name!@!.</h2>
+		</td>
+	</tr>
+
+	<tr>
+		<td class="montserratsemibold" style="font-family: Helvetica, Arial, sans-serif; font-size: 14px; font-weight: bold; padding-left: 0; padding-right: 0; padding-top: 0; color: #848484; text-align: center; padding-bottom: 15px; line-height: 1; text-transform: uppercase; letter-spacing: 1.25px;">
+			<?php echo date('l, F j, Y'); ?>
+		</td>
+	</tr>
+
+	<?php
+	$weather = get_weather( 'weather-today' );
+	if (!empty($weather)) :
+	?>
+		<tr>
+			<td style="font-family: Helvetica, Arial, sans-serif; font-size: 14px; padding-left: 0; padding-right: 0; padding-top: 0; color: #848484; text-align: center; padding-bottom: 0; line-height: 1;" align="center">
+				<img height="32" width="32" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $weather['today']['imgCode']; ?>.png" />
+				High <strong><?php echo $weather['today']['tempN']; ?>&deg;</strong>
+				<img height="32" width="32" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $weather['tonight']['imgCode']; ?>.png" />
+				Low <strong><?php echo $weather['tonight']['tempN']; ?>&deg;</strong>
+			</td>
+		</tr>
+	<?php endif; ?>
+</table>
--- includes/news/v2/mail/in-the-news.php
+++ includes/news/v2/mail/in-the-news.php
@@ -1,45 +1,46 @@
-<tr>
-  <td class="montserratbold" style="padding-top: 20px; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 20px; font-weight: bold; color: #000; text-transform: uppercase;" align="left">
-	UCF In the News
-  </td>
-</tr>
+<table width="100%" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width:100%; margin:0; background-color:#fff;">
+	<tr>
+		<td class="montserratbold" style="padding-top: 0; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 20px; font-weight: bold; color: #000; text-transform: uppercase;" align="left">
+			UCF In the News
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<?php
+			$external = get_in_the_news_stories();
 
-<tr>
-	<td>
-		<?php
-		$external = get_in_the_news_stories();
-
-		if( count( $external ) == 0 ) { ?>
-			<p>No stories found for today.</p>
-		<?php
-		} else {
-			?>
-			<ul style="list-style: none; padding-left: 0;">
+			if( count( $external ) == 0 ) { ?>
+				<p>No stories found for today.</p>
 			<?php
-			foreach( $external as $story ) :
+			} else {
+				?>
+				<ul style="list-style: none; padding-left: 0;">
+				<?php
+				foreach( $external as $story ) :
+				?>
+					<li style="padding-top: 8px; padding-bottom: 8px;" align="left">
+						<table>
+							<tbody>
+							<tr><td style="padding-bottom: 4px;">
+								<a class="montserratlight" style="font-family: Helvetica, Arial, sans-serif; font-size: 18px; letter-spacing: 1px;" href="<?php echo $story->url; ?>">
+									<?php echo $story->link_text; ?>
+								</a>
+							</td></tr>
+							<tr><td style="padding-bottom: 4px;">
+								<a class="montserratlight" style="font-family: Helvetica, Arial, sans-serif; font-size: 12px; color: #888; text-decoration: none; text-transform: uppercase;" href="<?php echo $story->url; ?>">
+									<?php echo $story->source; ?>
+								</a>
+							</td></tr>
+							</tbody>
+						</table>
+					</li>
+				<?php
+				endforeach;
+				?>
+				</ul>
+				<?php
+			}
 			?>
-				<li style="padding-top: 8px; padding-bottom: 8px;" align="left">
-                    <table>
-                        <tbody>
-                        <tr><td style="padding-bottom: 4px;">
-                            <a class="montserratlight" style="font-family: Helvetica, Arial, sans-serif; font-size: 18px; color: #000; font-weight: 100; letter-spacing: 1px; text-decoration: none;" href="<?php echo $story->url; ?>">
-                                <?php echo $story->link_text; ?>
-                            </a>
-                        </td></tr>
-                        <tr><td style="padding-bottom: 4px;">
-                            <a class="montserratbold" style="font-family: Helvetica, Arial, sans-serif; font-size: 14px; color: #888; text-decoration: none; text-transform: uppercase;" href="<?php echo $story->url; ?>">
-                                <?php echo $story->source; ?>
-                            </a>
-                        </td></tr>
-                        </tbody>
-                    </table>
-				</li>
-			<?php
-			endforeach;
-			?>
-			</ul>
-			<?php
-		}
-		?>
-	</td>
-</tr>
+		</td>
+	</tr>
+</table>
