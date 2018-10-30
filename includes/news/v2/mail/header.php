a0a749e003afcc8b4498074651ac5def23b122f2
Cadie Brown <cadie.brown@gmail.com>
2018-10-30 16:10:34 -0400

    Add different layouts for weather icons

--- includes/news/v2/mail/header.php
+++ includes/news/v2/mail/header.php
@@ -9,7 +9,7 @@
 		<td style="padding-top: 0; padding-left: 0; padding-right: 0;">
 			<table align="center" style="width: 100%; border-spacing: 0; border-collapse: collapse;"><tbody>
 				<tr>
-					<th class="columnCollapse" align="left" width="290" style="font-family: Helvetica, Arial, sans-serif; color: #848484; text-transform: uppercase; padding-left: 0; padding-right: 0; padding-top: 10px; padding-bottom: 0; vertical-align: top; text-align: center;">
+					<th class="columnCollapse" align="left" width="290" style="font-family: Helvetica, Arial, sans-serif; color: #848484; text-transform: uppercase; padding-left: 0; padding-right: 0; padding-top: 10px; padding-bottom: 0; vertical-align: middle; text-align: center;">
 						<table class="tableCollapse" style="width: 100%; border-spacing: 0; border-collapse: collapse;"><tbody>
 							<tr>
 								<td class="montserratlight">
@@ -18,20 +18,51 @@
 							</tr>
 						</table>
 					</th>
-					<th class="columnCollapse" align="right" width="290" style="font-family: Helvetica, Arial, sans-serif; color: #848484; padding-left: 0; padding-right: 0; padding-top: 10px; padding-bottom: 0; vertical-align: top; text-align: center;">
-						<table class="tableCollapse" style="width: 100%; border-spacing: 0; border-collapse: collapse;"><tbody>
+					<th class="columnCollapse" align="right" width="290" style="font-family: Helvetica, Arial, sans-serif; color: #848484; padding-left: 0; padding-right: 0; padding-top: 10px; padding-bottom: 0; vertical-align: middle; text-align: center;">
+						<table class="hidemobile" style="width: 100%; border-spacing: 0; border-collapse: collapse;"><tbody>
 							<tr>
-								<td class="montserratlight">
-									<?php
-									$weather = get_weather( 'weather-today' );
-									if (!empty($weather)) :
-									?>
-										<img height="25" width="25" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $weather['today']['imgCode']; ?>.png" />
-										<span style="font-weight: normal;">High</span> <strong><?php echo $weather['today']['tempN']; ?>&deg;</strong>
-										<img height="25" width="25" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $weather['tonight']['imgCode']; ?>.png" />
-										<span style="font-weight: normal;">Low</span> <strong><?php echo $weather['tonight']['tempN']; ?>&deg;</strong>
-									<?php endif; ?>
+							<?php
+							$weather = get_weather( 'weather-today' );
+							if ( ! empty( $weather ) ) :
+								$today_icon = get_weather_icon( $weather['today']['condition'] );
+								$tonight_icon = get_weather_icon( $weather['tonight']['condition'], true );
+							?>
+								<td valign="middle" style="">
+									<img height="25" width="25" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $today_icon; ?>.png" />
 								</td>
+								<td valign="middle">
+									<p style="margin-top: 0; margin-bottom: 0;"><span class="montserratlight">High</span> <span class="montserratsemibold" ><?php echo $weather['today']['tempN']; ?>&deg;</span></p>
+								</td>
+								<td valign="middle" style="">
+									<img height="25" width="25" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $tonight_icon; ?>.png" />
+								</td>
+								<td valign="middle">
+									<p style="margin-top: 0; margin-bottom: 0;"><span class="montserratlight">Low</span> <span class="montserratsemibold" ><?php echo $weather['tonight']['tempN']; ?>&deg;</span></p>
+								</td>
+							<?php endif; ?>
+							</tr>
+						</table>
+						<table class="showmobile" style="width: 100%; border-spacing: 0; border-collapse: collapse;"><tbody>
+							<tr>
+							<?php
+							$weather = get_weather( 'weather-today' );
+							if ( ! empty( $weather ) ) :
+								$today_icon = get_weather_icon( $weather['today']['condition'] );
+								$tonight_icon = get_weather_icon( $weather['tonight']['condition'], true );
+							?>
+								<td valign="middle" style="">
+									<img height="25" width="25" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $today_icon; ?>.png" />
+								</td>
+								<td valign="middle">
+									<p style="margin-top: 0; margin-bottom: 0;"><span class="montserratlight">High</span> <span class="montserratsemibold" ><?php echo $weather['today']['tempN']; ?>&deg;</span></p>
+								</td>
+								<td valign="middle" style="">
+									<img height="25" width="25" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/weather/<?php echo $tonight_icon; ?>.png" />
+								</td>
+								<td valign="middle">
+									<p style="margin-top: 0; margin-bottom: 0;"><span class="montserratlight">Low</span> <span class="montserratsemibold" ><?php echo $weather['tonight']['tempN']; ?>&deg;</span></p>
+								</td>
+							<?php endif; ?>
 							</tr>
 						</table>
 					</th>
