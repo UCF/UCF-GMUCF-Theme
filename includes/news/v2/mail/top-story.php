d2b317e91dd70d649a5ef65d78c73e02123bf7d1
Cadie Brown <cadie.brown@gmail.com>
2018-11-01 13:45:17 -0400

    Remove read more link from top story and make image, description and title clickable

--- includes/news/v2/mail/top-story.php
+++ includes/news/v2/mail/top-story.php
@@ -4,28 +4,27 @@ extract( get_top_story_details() );
 <table width="100%" border="0" align="center" cellpadding="0" bgcolor="#FFF" cellspacing="0" style="width:100%; margin:0; background-color:#fff;">
 	<tr>
 		<td style="padding-bottom: 20px; padding-top: 15px; padding-left: 0; padding-right: 0; border: 0;">
-			<table class="tableCollapse" width="600" border="0" align="center" style="border-spacing: 0; border-collapse: collapse;">
-				<tbody>
-					<tr>
-						<td style="padding-left: 0; padding-right: 0;">
-							<a href="<?php echo $read_more_uri; ?>">
+			<a href="<?php echo $read_more_uri; ?>" style="color: #000; text-decoration: none;">
+				<table class="tableCollapse" width="600" border="0" align="center" style="border-spacing: 0; border-collapse: collapse;">
+					<tbody>
+						<tr>
+							<td style="padding-left: 0; padding-right: 0;">
 								<img class="responsiveimg" border="0" style="border:none;" src="<?php echo $thumbnail_src; ?>" />
-							</a>
-						</td>
-					</tr>
-					<tr>
-						<td class="montserratsemibold" style="padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 24px; font-weight: 500; padding-top: 15px; padding-bottom: 10px; line-height: 1.1; color: #000; text-align: left;" align="left">
-						<a href="<?php echo $read_more_uri; ?>" style="color: #000; text-decoration: none;"><?php echo $story_title; ?></a>
-						</td>
-					</tr>
-					<tr>
-						<td class="montserratlight" style="padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 1.4; color: #000; text-align: left;" align="left">
-						<?php echo $story_description; ?>
-						<a href="<?php echo $read_more_uri; ?>" style="color: #006699; text-decoration: underline;"> Read More</a>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+							</td>
+						</tr>
+						<tr>
+							<td class="montserratsemibold" style="padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 24px; font-weight: 500; padding-top: 15px; padding-bottom: 10px; line-height: 1.1; color: #000; text-align: left;" align="left">
+							<?php echo $story_title; ?>
+							</td>
+						</tr>
+						<tr>
+							<td class="montserratlight" style="padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 1.4; color: #000; text-align: left;" align="left">
+							<?php echo $story_description; ?>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</a>
 		</td>
 	</tr>
 </table>
