286a9d1672fd5297c824684abf4d598c20d7ef74
Cadie Brown <cadie.brown@gmail.com>
2018-11-02 17:18:45 -0400

    Start adjusting social icons

--- functions.php
+++ functions.php
@@ -1029,10 +1029,11 @@ function display_social_share( $permalink, $title ) {
 	$title = urlencode( $title );
 	?>
 	<tr>
-		<td class="montserratlight" style="padding-top: 10px; padding-left: 0; padding-right: 0; text-align: center;" align="center">
-			<a href="http://www.facebook.com/sharer.php?u=<?php echo $permalink; ?>"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/facebook-square.png" alt="Share on Facebook" width="80" height="25"></a>
-			<a href="https://twitter.com/intent/tweet?text=<?php echo $title; ?>&url=<?php echo $permalink; ?>"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/twitter-square.png" alt="Share on Twitter" width="80" height="25"></a>
-			<a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo $permalink; ?>&title=<?php echo $title; ?>&source=today.ucf.edu"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/linkedin-square.png" alt="Share on LinkedIn" width="80" height="25"></a>
+		<td class="montserratlight" style="padding-top: 10px; padding-left: 0; padding-right: 0; text-align: right;" align="right">
+			<span>Share: </span>
+			<a href="http://www.facebook.com/sharer.php?u=<?php echo $permalink; ?>"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/facebook-share.png" alt="Share on Facebook" width="20" height="20"></a>
+			<a href="https://twitter.com/intent/tweet?text=<?php echo $title; ?>&url=<?php echo $permalink; ?>"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/twitter-share.png" alt="Share on Twitter" width="20" height="20"></a>
+			<a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo $permalink; ?>&title=<?php echo $title; ?>&source=today.ucf.edu"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/linkedin-share.png" alt="Share on LinkedIn" width="20" height="20"></a>
 		</td>
 	</tr>
 	<?php
