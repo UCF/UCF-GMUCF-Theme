112316288f9fd19d1da6293a9dc770ae7b7f9186
Cadie Brown <cadie.brown@gmail.com>
2018-11-13 11:21:27 -0500

    Copy mail files into browser directory

---
+++ includes/news/browser/footer.php
@@ -0,0 +1,56 @@
+<tr>
+	<td style="padding-bottom: 20px; padding-top: 60px; padding-left: 0; padding-right: 0; text-align: center;" align="center">
+		<a href="https://www.facebook.com/ucf/">
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/facebook-circle.png" alt="Facebook" align="center" class="float-center" style="display:inline-block; padding-right: 8px;" width="42" height="42"></a>
+		<a href="https://www.twitter.com/UCF/">
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/twitter-circle.png" alt="Twitter" align="center" class="float-center" style="display:inline-block; padding-right: 8px;" width="42" height="42"></a>
+		<a href="https://www.instagram.com/ucf.edu">
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/instagram-circle.png" alt="Instagram" align="center" class="float-center" style="display:inline-block; padding-right: 8px;" width="42" height="42"></a>
+		<a href="https://www.linkedin.com/edu/university-of-central-florida-18118">
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/linkedin-circle.png" alt="LinkedIn" align="center" class="float-center" style="display:inline-block; padding-right: 8px;" width="42" height="42"></a>
+		<a href="https://www.youtube.com/user/UCF/">
+			<img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/social/youtube-circle.png" alt="YouTube" align="center" class="float-center" style="display:inline-block;" width="42" height="42"></a>
+	</td>
+</tr>
+
+<tr>
+	<td style="padding-bottom: 20px; padding-top: 20px; padding-left: 0; padding-right: 0; text-align: center" align="center">
+			<a href="https://www.ucf.edu"><img src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/static/img/ucf-tab.png" width="100" height="135" alt="UCF Tab" align="center" class="float-center"></a>
+	</td>
+</tr>
+
+<tr>
+	<td class="montserratbold" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: bold; text-transform: uppercase; color: #000; letter-spacing: 1.25px;" align="center">
+		<a style="text-decoration: none; color: #000;" href="https://www.ucf.edu/">University of Central Florida</a>
+	</td>
+</tr>
+
+<tr>
+	<td class="montserratlight" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+		4000 Central Florida Blvd.
+	</td>
+</tr>
+
+<tr>
+	<td class="montserratlight" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+		Orlando, FL 32816
+	</td>
+</tr>
+
+<tr>
+	<td class="montserratlight" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+		407-823-2000
+	</td>
+</tr>
+
+<tr>
+	<td class="montserratbold" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: bold; color: #000; text-transform: uppercase;" align="center">
+		<a href="https://today.ucf.edu/suggest-a-story/">Suggest a Story</a> <span style="color: #aaa; font-weight: normal; padding-left: 5px; padding-right: 5px;"> | </span> <a href="https://today.ucf.edu/tell-us-what-you-think/">Tell Us What You Think</a>
+	</td>
+</tr>
+
+<tr>
+	<td class="montserratlight" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;" align="center">
+		!@!UNSUBSCRIBE!@! from this email.
+	</td>
+</tr>
