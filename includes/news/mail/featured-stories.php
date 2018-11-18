172dea1d08be0b6e2c83447310cb57a172aaae2c
cadie <cadie.brown@gmail.com>
2018-11-18 18:02:01 -0500

    Style guide fixes and indentation fix

--- includes/news/mail/featured-stories.php
+++ includes/news/mail/featured-stories.php
@@ -4,53 +4,51 @@
 			<table align="center" style="width: 100%; border-spacing: 0; border-collapse: collapse;">
 				<tbody>
 					<tr>
-<?
-	$limit = 4;
-	$featured_stories = get_featured_stories_details( $limit );
-	$total = count( $featured_stories );
+					<?php
+					$limit = 4;
+					$featured_stories = get_featured_stories_details( $limit );
+					$total = count( $featured_stories );
 
-	foreach( $featured_stories as $index => $details ) {
-		extract( $details );
-		if( $index == $limit || $index == $total ) break;
-		if( $index > 0 && $index % 2 == 0 ) echo "</tr><tr>";
-		?>
-		<th class="columnCollapse" align="left" width="290" style="font-family: Helvetica, Arial, sans-serif; padding-left: 10px; padding-right: 10px; padding-top: 0; padding-bottom: 45px; vertical-align: top; text-align: center;">
-			<table class="tableCollapse" width="100%" style="width: 100%; border-spacing: 0; border-collapse: collapse;"><tbody>
-				<tr>
-					<td>
-						<table width="100%" border="0" align="center" style="border-spacing: 0; border-collapse: collapse;">
-							<tbody>
+					foreach( $featured_stories as $index => $details ) :
+						extract( $details );
+						if( $index == $limit || $index == $total ) break;
+						if( $index > 0 && $index % 2 == 0 ) echo "</tr><tr>";
+					?>
+						<th class="columnCollapse" align="left" width="290" style="font-family: Helvetica, Arial, sans-serif; padding-left: 10px; padding-right: 10px; padding-top: 0; padding-bottom: 45px; vertical-align: top; text-align: center;">
+							<table class="tableCollapse" width="100%" style="width: 100%; border-spacing: 0; border-collapse: collapse;"><tbody>
 								<tr>
-									<td style="padding-left: 0; padding-right: 0;">
-										<a href="<?php echo $permalink; ?>" style="color: #000; text-decoration: none;">
-											<img class="responsiveimg" border="0" width="290" style="border:none;" src="<?php echo $image; ?>" />
-										</a>
+									<td>
+										<table width="100%" border="0" align="center" style="border-spacing: 0; border-collapse: collapse;">
+											<tbody>
+												<tr>
+													<td style="padding-left: 0; padding-right: 0;">
+														<a href="<?php echo $permalink; ?>" style="color: #000; text-decoration: none;">
+															<img class="responsiveimg" border="0" width="290" style="border:none;" src="<?php echo $image; ?>" />
+														</a>
+													</td>
+												</tr>
+												<tr>
+													<td class="montserratsemibold" style="padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 19px; font-weight: 500; padding-top: 20px; padding-bottom: 15px; line-height: 1.3; color: #000; text-align: left;" align="left">
+														<a href="<?php echo $permalink; ?>" style="color: #000; text-decoration: none;">
+															<?php echo $title; ?>
+														</a>
+													</td>
+												</tr>
+												<tr>
+													<td class="montserratlight" style="padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 1.6; color: #000; text-align: left;" align="left">
+														<a href="<?php echo $permalink; ?>" style="color: #000; text-decoration: none;">
+															<?php echo strip_tags( $description ); ?>
+														</a>
+													</td>
+												</tr>
+											</tbody>
+										</table>
 									</td>
 								</tr>
-								<tr>
-									<td class="montserratsemibold" style="padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 19px; font-weight: 500; padding-top: 20px; padding-bottom: 15px; line-height: 1.3; color: #000; text-align: left;" align="left">
-										<a href="<?php echo $permalink; ?>" style="color: #000; text-decoration: none;">
-											<?php echo $title; ?>
-										</a>
-									</td>
-								</tr>
-								<tr>
-									<td class="montserratlight" style="padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 1.6; color: #000; text-align: left;" align="left">
-										<a href="<?php echo $permalink; ?>" style="color: #000; text-decoration: none;">
-											<?php echo strip_tags( $description ); ?>
-										</a>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</td>
-				</tr>
-				<?php echo display_social_share( $permalink, $title ); ?>
-			</table>
-		</th>
-	<?php
-	}
-?>
+								<?php echo display_social_share( $permalink, $title ); ?>
+							</table>
+						</th>
+					<?php endforeach; ?>
 					</tr>
 				</tbody>
 			</table>
