06461c9ebabbd0b5eeae23d8823b8b1efea4215c
cadie <cadie.brown@gmail.com>
2018-11-18 02:24:15 -0500

    Require email markup functions file

--- functions.php
+++ functions.php
@@ -56,10 +56,11 @@ define('HTTP_TIMEOUT', 5); //seconds
 # Custom Image Sizes
 add_image_size('top_story', 600, 308, True);
 
-require_once('functions-base.php');     # Base theme functions
-require_once('custom-post-types.php');  # Where per theme post types are defined
-require_once('shortcodes.php');         # Per theme shortcodes
-require_once('functions-admin.php');    # Admin/login functions
+require_once('functions-base.php');         # Base theme functions
+require_once('custom-post-types.php');      # Where per theme post types are defined
+require_once('shortcodes.php');             # Per theme shortcodes
+require_once('functions-admin.php');        # Admin/login functions
+require_once('functions-email-markup.php'); # Email layout functions
 
 
 /**
