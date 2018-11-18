86fe8735c5d8a19b78b7f6a94b27311faced913f
cadie <cadie.brown@gmail.com>
2018-11-18 03:15:47 -0500

    Create backup email content

---
+++ includes/news/mail/backup-email-content.php
@@ -0,0 +1,11 @@
+<tr>
+	<td style="padding-bottom: 10px; padding-left: 0; padding-right: 0; text-align: center;">
+		<?php echo get_template_part( 'includes/news/mail/top-story' ); ?>
+	</td>
+</tr>
+<?php echo get_template_part( 'includes/news/mail/alert' ); ?>
+<tr>
+	<td style="padding-bottom: 0; padding-left: 0; padding-right: 0; text-align: center;">
+		<?php echo get_template_part( 'includes/news/mail/featured-stories' ); ?>
+	</td>
+</tr>
