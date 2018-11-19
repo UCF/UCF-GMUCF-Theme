41eef04ca0a4e1d3e6841eeb66f62670538b65af
cadie <cadie.brown@gmail.com>
2018-11-18 21:30:34 -0500

    Create text version of email content

--- includes/news/text/base.php
+++ includes/news/text/base.php
@@ -2,52 +2,50 @@
 UCF TODAY
 <?php echo date( 'l, F j, Y' ) ?>
 
-<?php $weather = get_weather( 'weather-today' ); if ( !empty( $weather ) ) { ?>
+<?php 
+$weather = get_weather( 'weather-today' ); 
+if ( !empty( $weather ) ) : ?>
 High: <?php echo $weather['today']['tempN']; ?> Low: <?php echo $weather['tonight']['tempN'] ?>
-<?php } ?>
-
-<?php $top_story_details = get_top_story_details(); ?>
+<?php endif; ?>
 
 
-TODAY'S TOP STORIES
-
-<?php echo strip_tags( $top_story_details['story_title'] ); ?>
-
-<?php echo strip_tags( $top_story_details['story_description'] ); ?>
-
-<?php echo $top_story_details['read_more_uri']; ?>
 
 
-<?php foreach( get_featured_stories_details( 4 ) as $detail ) { ?>
-<?php echo strip_tags( $detail['title'] ); ?>
+<?php
+$gmucf_content = get_gmucf_email_options_feed_values();
+$send_date     = $gmucf_content->gmucf_email_send_date;
 
-<?php echo strip_tags( $detail['description'] ); ?>
-
-<?php echo $detail['permalink']; ?>
-
-
-<?php } ?>
-
+if ( $send_date === date( 'm/d/Y' ) ) {
+    echo get_template_part( 'includes/news/text/email-text' );
+} else {
+    echo get_template_part( 'includes/news/text/backup-email-text' );
+}
+?>
 
 UCF IN THE NEWS
-
-<?php foreach( get_in_the_news_stories() as $story ) { ?>
-<?php echo strip_tags( $story->link_text ); ?> (<?php echo trim( strip_tags( $story->source ) ); ?>)
-<?php echo $story->url; ?>
-
-
-<?php } ?>
+========================
+<?php foreach( get_in_the_news_stories() as $story ) : ?>
+- <?php echo strip_tags( $story->link_text ); ?> (<?php echo trim( strip_tags( $story->source ) ); ?>)
+  <?php echo $story->url; ?>
 
 
+<?php endforeach; ?>
+
+<?php 
+$announcements = get_announcement_details();
+
+if ( count( $announcements ) != 0 ) :
+?>
 ANNOUNCEMENTS
+========================
+<?php foreach( $announcements as $announcement ) : ?>
+- <?php echo strip_tags( $announcement['title'] ); ?>
 
-<?php foreach( get_announcement_details() as $announcement ) { ?>
-<?php echo strip_tags( $announcement['title'] ); ?>
-
-<?php echo $announcement['permalink']; ?>
+  <?php echo $announcement['permalink']; ?>
 
 
-<?php } ?>
+<?php endforeach; ?>
+<?php endif; ?>
 
 
 UNIVERSITY OF CENTRAL FLORIDA
