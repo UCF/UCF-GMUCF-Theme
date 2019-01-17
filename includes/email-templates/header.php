eccbc7c1e17e46517fb8ba723571273b5d120660
RJ Bruneel <rj.bruneel@ucf.edu>
2019-01-17 13:24:22 -0500

    converted tabs to spaces

--- includes/email-templates/header.php
+++ includes/email-templates/header.php
@@ -2,41 +2,46 @@
 <html lang="en">
 
 <head>
-	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-	<meta name="format-detection" content="telephone=no">
-	<meta name="viewport" content="initial-scale=1.0">
-	<!-- So that mobile webkit will display zoomed in -->
-	<link rel="stylesheet" type="text/css" href="https://s3.amazonaws.com/web.ucf.edu/email/common-assets/stylesheet.css">
-	<title><?php the_title(); ?></title>
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="viewport" content="initial-scale=1.0">
 
-	<?php get_template_part( 'includes/email-templates/css' ); ?>
+  <!-- So that mobile webkit will display zoomed in -->
+  <link rel="stylesheet" type="text/css" href="https://s3.amazonaws.com/web.ucf.edu/email/common-assets/stylesheet.css">
+  <title>
+    <?php the_title(); ?>
+  </title>
 
-	<?php if( is_admin_bar_showing() ) : ?>
-		<style>
-			body {
-				margin-top: 46px;
-			}
-			@media (min-width: 783px) {
-				body {
-					margin-top: 30px;
-				}
-			}
-		</style>
-	<?php endif; ?>
+  <?php get_template_part( 'includes/email-templates/css' ); ?>
+
+  <?php if ( is_admin_bar_showing() ) : ?>
+  <style>
+    body {
+      margin-top: 46px;
+    }
+
+    @media (min-width: 783px) {
+      body {
+        margin-top: 30px;
+      }
+    }
+  </style>
+  <?php endif; ?>
 </head>
 
 <body>
 
-<table class="t640" width="640" align="center">
-	<tbody>
-		<tr>
-			<td style="padding: 0;">
-				<img src="https://s3.amazonaws.com/web.ucf.edu/email/postmaster-templates/pro-banner.png" alt="UCF banner" class="responsiveimg">
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<table class="t564" width="564" align="center" style="padding-top: 15px;">
-					<tbody>
-						<tr>
-							<td style="padding-bottom: 0;">
+  <table class="t640" width="640" align="center">
+    <tbody>
+      <tr>
+        <td style="padding: 0;">
+          <img src="https://s3.amazonaws.com/web.ucf.edu/email/postmaster-templates/pro-banner.png" alt="UCF banner"
+            class="responsiveimg">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <table class="t564" width="564" align="center" style="padding-top: 15px;">
+          <tbody>
+            <tr>
+              <td style="padding-bottom: 0;">
--- includes/email-templates/president/footer.php
+++ includes/email-templates/president/footer.php
@@ -1,140 +1,140 @@
-						</td>
-					</tr>
-						<tr>
-							<td class="ccollapse100pb" style="width: 100px; padding-bottom: 30px;" width="100">
-								<img class="responsiveimgmw" alt="Dale signature" src="https://s3.amazonaws.com/web.ucf.edu/email/postmaster-templates/Dale-signature-2.jpg"
-									style="width: 100px;" width="100">
-							</td>
-						</tr>
-						<tr>
-							<td class="ccollapse100">
-								<table class="tcollapse100" style="width: 100%;">
-									<tbody>
-										<tr>
-											<td class="ccollapse100" valign="top" style="width: 100px; text-align: left; padding-bottom: 15px;"
-												width="100">
-												<img alt="Dale Whittaker" src="https://s3.amazonaws.com/web.ucf.edu/email/postmaster-templates/pro-provost2.jpg"
-													style="display: inline-block; padding-right: 20px;">
-											</td>
-											<td class="ccollapse100">
-												<table class="tcollapse100" style="width: 100%;">
-													<tbody>
-														<tr>
-															<td class="montserratbold" style="width: 100%; font-size: 16px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; padding-bottom: 2px;">Dale
-																Whittaker, Ph.D.</td>
-														</tr>
-														<tr>
-															<td class="montserratlight" style="width: 100%; font-size: 15px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; padding-bottom: 10px;">President</td>
-														</tr>
-														<tr>
-															<td>
-																<table>
-																	<tbody>
-																		<tr>
-																			<td class="ccollapse100" style="padding-bottom: 10px; padding-right: 15px;">
-																				<table>
-																					<tbody>
-																						<tr>
-																							<td style="padding-right: 4px; padding-bottom: 6px;">
-																								<img alt="Twitter" src="https://s3.amazonaws.com/web.ucf.edu/email/common-assets/twitter-2.png"
-																									width="25" height="25">
-																							</td>
-																							<td>
-																								<a href="https://twitter.com/ucfwhittaker"
-																									class="montserratbold" style="display: inline-block; font-size: 15px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; text-decoration: none;">
-																									@UCFWhittaker
-																								</a>
-																							</td>
-																						</tr>
-																					</tbody>
-																				</table>
-																			</td>
-																			<td class="ccollapse100" style="padding-bottom: 10px;">
-																				<table>
-																					<tbody>
-																						<tr>
-																							<td style="padding-right: 4px; padding-bottom: 6px;">
-																								<img alt="Instagram" src="https://s3.amazonaws.com/web.ucf.edu/email/common-assets/instagram-2.png"
-																									width="25" height="25">
-																							</td>
-																							<td>
-																								<a href="https://www.instagram.com/ucfwhittaker/"
-																									class="montserratbold" style="display: inline-block; font-size: 15px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; text-decoration: none;">
-																									@UCFWhittaker
-																								</a>
-																							</td>
-																						</tr>
-																					</tbody>
-																				</table>
-																			</td>
-																		</tr>
-																	</tbody>
-																</table>
-															</td>
-														</tr>
-													</tbody>
-												</table>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-							</td>
-						</tr>
-						<tr>
-							<td class="ccollapse100">
-								<table style="width: 100%;" align="center">
-									<tbody>
-										<tr>
-											<td style="padding-top: 15px; padding-bottom: 20px; padding-left: 0; padding-right: 0; width:100%;"
-												align="center">
-												<hr style="border: none; border-top: 1px solid lightgrey;">
-											</td>
-										</tr>
-									</tbody>
-								</table>
-							</td>
-						</tr>
-						<tr>
-							<td class="ccollapse100">
-								<table style="font-family: Helvetica, Arial, sans-serif; font-size: 13px; line-height: 1.4em;" align="center">
-									<tbody>
-										<tr>
-											<td class="montserratbold" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: bold; text-transform: uppercase; color: #000; letter-spacing: 1.25px;"
-												align="center">
-												<a style="text-decoration: none; color: #000;" href="https://www.ucf.edu/">University of
-													Central Florida</a>
-											</td>
-										</tr>
+            </td>
+          </tr>
+            <tr>
+              <td class="ccollapse100pb" style="width: 100px; padding-bottom: 30px;" width="100">
+                <img class="responsiveimgmw" alt="Dale signature" src="https://s3.amazonaws.com/web.ucf.edu/email/postmaster-templates/Dale-signature-2.jpg"
+                  style="width: 100px;" width="100">
+              </td>
+            </tr>
+            <tr>
+              <td class="ccollapse100">
+                <table class="tcollapse100" style="width: 100%;">
+                  <tbody>
+                    <tr>
+                      <td class="ccollapse100" valign="top" style="width: 100px; text-align: left; padding-bottom: 15px;"
+                        width="100">
+                        <img alt="Dale Whittaker" src="https://s3.amazonaws.com/web.ucf.edu/email/postmaster-templates/pro-provost2.jpg"
+                          style="display: inline-block; padding-right: 20px;">
+                      </td>
+                      <td class="ccollapse100">
+                        <table class="tcollapse100" style="width: 100%;">
+                          <tbody>
+                            <tr>
+                              <td class="montserratbold" style="width: 100%; font-size: 16px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; padding-bottom: 2px;">Dale
+                                Whittaker, Ph.D.</td>
+                            </tr>
+                            <tr>
+                              <td class="montserratlight" style="width: 100%; font-size: 15px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; padding-bottom: 10px;">President</td>
+                            </tr>
+                            <tr>
+                              <td>
+                                <table>
+                                  <tbody>
+                                    <tr>
+                                      <td class="ccollapse100" style="padding-bottom: 10px; padding-right: 15px;">
+                                        <table>
+                                          <tbody>
+                                            <tr>
+                                              <td style="padding-right: 4px; padding-bottom: 6px;">
+                                                <img alt="Twitter" src="https://s3.amazonaws.com/web.ucf.edu/email/common-assets/twitter-2.png"
+                                                  width="25" height="25">
+                                              </td>
+                                              <td>
+                                                <a href="https://twitter.com/ucfwhittaker"
+                                                  class="montserratbold" style="display: inline-block; font-size: 15px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; text-decoration: none;">
+                                                  @UCFWhittaker
+                                                </a>
+                                              </td>
+                                            </tr>
+                                          </tbody>
+                                        </table>
+                                      </td>
+                                      <td class="ccollapse100" style="padding-bottom: 10px;">
+                                        <table>
+                                          <tbody>
+                                            <tr>
+                                              <td style="padding-right: 4px; padding-bottom: 6px;">
+                                                <img alt="Instagram" src="https://s3.amazonaws.com/web.ucf.edu/email/common-assets/instagram-2.png"
+                                                  width="25" height="25">
+                                              </td>
+                                              <td>
+                                                <a href="https://www.instagram.com/ucfwhittaker/"
+                                                  class="montserratbold" style="display: inline-block; font-size: 15px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; text-decoration: none;">
+                                                  @UCFWhittaker
+                                                </a>
+                                              </td>
+                                            </tr>
+                                          </tbody>
+                                        </table>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <td class="ccollapse100">
+                <table style="width: 100%;" align="center">
+                  <tbody>
+                    <tr>
+                      <td style="padding-top: 15px; padding-bottom: 20px; padding-left: 0; padding-right: 0; width:100%;"
+                        align="center">
+                        <hr style="border: none; border-top: 1px solid lightgrey;">
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <td class="ccollapse100">
+                <table style="font-family: Helvetica, Arial, sans-serif; font-size: 13px; line-height: 1.4em;" align="center">
+                  <tbody>
+                    <tr>
+                      <td class="montserratbold" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: bold; text-transform: uppercase; color: #000; letter-spacing: 1.25px;"
+                        align="center">
+                        <a style="text-decoration: none; color: #000;" href="https://www.ucf.edu/">University of
+                          Central Florida</a>
+                      </td>
+                    </tr>
 
-										<tr>
-											<td class="montserratlight" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;"
-												align="center">
-												4000 Central Florida Blvd.
-											</td>
-										</tr>
+                    <tr>
+                      <td class="montserratlight" style="text-align: center; padding-bottom: 2px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;"
+                        align="center">
+                        4000 Central Florida Blvd.
+                      </td>
+                    </tr>
 
-										<tr>
-											<td class="montserratlight" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;"
-												align="center">
-												Orlando, FL 32816
-											</td>
-										</tr>
+                    <tr>
+                      <td class="montserratlight" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;"
+                        align="center">
+                        Orlando, FL 32816
+                      </td>
+                    </tr>
 
-										<tr>
-											<td class="montserratlight" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;"
-												align="center">
-												!@!UNSUBSCRIBE!@! from this email.
-											</td>
-										</tr>
-									</tbody>
-								</table>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</td>
-		</tr>
-	</tbody>
+                    <tr>
+                      <td class="montserratlight" style="text-align: center; padding-bottom: 20px; padding-left: 0; padding-right: 0; font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: #000;"
+                        align="center">
+                        !@!UNSUBSCRIBE!@! from this email.
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </tbody>
 </table>
 <?php wp_footer(); ?>
 </body>
