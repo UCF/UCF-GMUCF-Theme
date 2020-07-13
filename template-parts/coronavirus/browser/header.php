927ca577ed7825617f4b499a19603583ca43dfa6
Jo Dickson <jogreybill@gmail.com>
2020-07-13 12:37:15 -0400

    Initial setup for cv emails

---
+++ template-parts/coronavirus/browser/header.php
@@ -0,0 +1,8 @@
+<?php
+namespace GMUCF\Theme\TemplateParts\Coronavirus\Browser\Header;
+use GMUCF\Theme\Includes\Coronavirus;
+
+
+$current_date = current_datetime();
+?>
+TODO header
---
+++ template-parts/coronavirus/mail/footer.php
@@ -0,0 +1,5 @@
+<?php
+namespace GMUCF\Theme\TemplateParts\Coronavirus\Mail\Footer;
+use GMUCF\Theme\Includes\Coronavirus;
+?>
+TODO mail footer
