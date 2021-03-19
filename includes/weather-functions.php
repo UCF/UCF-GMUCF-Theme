4c70097bdc186c89d011ced02bb7e69ba7b753e9
Jim Barnes <jimbarnesdeveloper@gmail.com>
2021-03-19 12:09:02 -0400

    Added a microtime cache buster to the weather data fetch

--- includes/weather-functions.php
+++ includes/weather-functions.php
@@ -38,9 +38,12 @@ function get_weather( $cache_key ) {
 
 		if ( $json_url ) {
 			// Setup curl request and execute. Log errors if necessary.
+
+			$dt = microtime();
+
 			$ch = curl_init();
 			$options = array(
-				CURLOPT_URL            => $json_url,
+				CURLOPT_URL            => "$json_url?request_time=$dt",
 				CURLOPT_CONNECTTIMEOUT => get_option( 'weather_service_timeout' ),
 				CURLOPT_RETURNTRANSFER => true
 			);
