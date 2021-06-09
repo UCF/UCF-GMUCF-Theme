299b051b40d037542db13e32ae352b0365ae57a9
Jo Dickson <jogreybill@gmail.com>
2021-06-09 11:31:58 -0400

    Fixed font family, alignment issues of todays date in outlook

--- template-parts/news/mail/header.php
+++ template-parts/news/mail/header.php
@@ -11,7 +11,7 @@ $current_date = current_datetime();
 	</tr>
 
 	<tr>
-		<td class="montserratlight" style="padding-top: 5px; padding-left: 0; padding-right: 0; color: #757575; text-transform: uppercase; font-weight: 400; font-size: 14px; line-height: 20px;">
+		<td class="montserratlight" style="font-family: Helvetica, Arial, sans-serif; text-align: center; padding-top: 5px; padding-left: 0; padding-right: 0; color: #757575; text-transform: uppercase; font-weight: 400; font-size: 14px; line-height: 20px;">
 			<?php echo $current_date->format( 'l, F j, Y' ); ?>
 		</td>
 	</tr>
